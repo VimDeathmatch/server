e542fc6dc34cbac624b0c5813fb1d43a4d628c43
Matt Conde <matt.conde@gmail.com>
2020-10-23 22:42:12 +0100

    add simple rename puzzle

---
+++ server/src/puzzles/simple-rename.js
@@ -0,0 +1,23 @@
+module.exports.start = function () {
+    const BarContainer = styled`
+        width: 100%;
+        padding: 0 20px;
+    `;
+    const Bar = (props) => (
+        <BarContainer>
+            <h1>foo {props.bar}</h1>
+        </BarContainer>
+    );
+};
+
+module.exports.end = function () {
+    const FooContainer = styled`
+        width: 100%;
+        padding: 0 20px;
+    `;
+    const Foo = (props) => (
+        <FooContainer>
+            <h1>bar {props.foo}</h1>
+        </FooContainer>
+    );
+}; 
