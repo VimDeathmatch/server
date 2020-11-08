ada449a20dbf3247ca90a13c35891bb0254b03a3
Michael Paulson <mpaulson@netflix.com>
2020-11-07 18:31:53 -0800

    feat(game play)

---
+++ src/game/behavioral-tree/states/__tests__/player-play-game.ts
@@ -0,0 +1,82 @@
+import pino from "pino";
+
+import MockSocket, { trashSocket, readySocket } from "../../../../__mocks__/socket";
+
+import { createPlayer } from "../../../player";
+import PlayGameNode, { playGame } from "../player-play-game";
+import { GameConfig, Player } from "../../../../types";
+
+const logger = pino({name: "PlayGameNode.test"});
+function wait(ms: number): Promise<void> {
+    return new Promise(res => setTimeout(res, ms));
+}
+
+function createGameConfig(arg: object = {}): GameConfig {
+    return {
+        logger,
+        maxPlayers: 1,
+        maxPlayTime: 42069,
+        puzzle: {
+            start: [],
+            end: [],
+            filetype: "ts",
+        },
+        ...arg
+    };
+}
+
+function createState(arg: object = {}): {[keys: string]: boolean} {
+    return {
+        timedout: false,
+        failed: false,
+        disconnected: false,
+        finished: false,
+        ...arg
+    };
+}
+
+function assignToPlayer(player: Player, arg: {[keys: string]: boolean}) {
+    Object.keys(arg).forEach(k => {
+        player[k] = arg[k];
+    });
+}
+
+describe("PlayGameNode", function() {
+    it("tests all the conditions for player shouldEnter", async function() {
+        const socket = new MockSocket();
+        let player = createPlayer(socket as any, logger);
+        const pgn = new PlayGameNode(createGameConfig());
+        const states = [
+            createState({timedout: true}),
+            createState({failed: true}),
+            createState({disconnected: true}),
+            createState({finished: true}),
+            createState(),
+        ];
+
+        const shouldEnterExpects = [false, false, false, false, true];
+        for (let i = 0; i < states.length; ++i) {
+            assignToPlayer(player, states[i]);
+            expect(await pgn.shouldEnter([player])).toEqual(shouldEnterExpects[i]);
+        }
+    });
+
+    it("Ensures run only runs once.", async function() {
+        const socket = new MockSocket();
+        let player = createPlayer(socket as any, logger);
+        const pgn = new PlayGameNode(createGameConfig());
+        jest.spyOn(player, "send");
+
+        expect(await pgn.shouldEnter([player])).toEqual(true);
+        await pgn.run([player]);
+        expect(player.send).toHaveBeenCalledTimes(1);
+
+        expect(await pgn.shouldEnter([player])).toEqual(true);
+        await pgn.run([player]);
+        expect(player.send).toHaveBeenCalledTimes(1);
+    });
+});
+
+
+describe("playGame", function() {
+});
---
+++ src/game/behavioral-tree/states/player-play-game.ts
@@ -0,0 +1,100 @@
+import pino from "pino";
+
+import { Player, GameConfig, BehavorialNode } from "../../../types";
+import { PlayerStats } from "../../score";
+import PlayerFailure from "../../errors/player-failure";
+
+// send the game message
+//        -> some amount of time to complete it.
+//        -> send back their results
+//        -> analyze them results
+//        -> zip
+//        -> transition
+function wait(ms: number): Promise<void> {
+    return new Promise(res => setTimeout(res, ms));
+}
+
+export async function playGame(config: GameConfig, player: Player, logger: pino.Logger): Promise<void> {
+    try {
+        await player.send("start-game", {
+            ...config.puzzle,
+            editable: true,
+        });
+
+        const results: [string, string] | null = await Promise.race([
+            player.getNextCommand("finished"),
+            wait(config.maxPlayTime).then(() => null),
+        ]);
+
+        if (results === null) {
+            logger.warn("Player has timedout", {player, config});
+            player.timedout = true;
+            return;
+        }
+
+        logger.info("Player has successed", {results});
+        const stats = new PlayerStats(results[1]);
+
+        if (stats.failed) {
+            player.failed = true;
+        } else {
+            player.stats.calculateScore(stats);
+            player.finished = true;
+        }
+
+    } catch (e) {
+        logger.fatal(e, "There was an error attempting to play the game", {
+            config,
+            player,
+        });
+        throw new PlayerFailure(player, e);
+    }
+}
+
+
+export default class PlayGameNode implements BehavorialNode {
+    private logger: pino.Logger;
+    private gamePlayStarted: boolean
+
+    constructor(private config: GameConfig) {
+        this.logger = config.logger.child({name: "PlayGameNode"});
+        this.gamePlayStarted = false;
+    }
+
+    async enter(): Promise<void> { }
+
+    async shouldEnter(players: Player[]): Promise<boolean> {
+        let failures = false;
+        let allFinished = true;
+        for (let i = 0; !failures && i < players.length; ++i) {
+            const player = players[i];
+            failures = player.failed || player.disconnected || player.timedout;
+            allFinished = allFinished && player.finished;
+        }
+
+        return !failures && !allFinished;
+    }
+
+    async run(players: Player[]): Promise<void> {
+        if (this.gamePlayStarted) {
+            return;
+        }
+
+        players.forEach(async (p) => {
+            try {
+                await playGame(this.config, p, this.logger);
+            } catch (e) {
+                e = e as PlayerFailure;
+                this.logger.fatal(e.reason, "A player has failed.", {
+                    player: e.player,
+                    config: this.config,
+                });
+            }
+        });
+
+        this.gamePlayStarted = true;
+    }
+
+    async exit(): Promise<void> { }
+}
+
